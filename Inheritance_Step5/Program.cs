cd69ebd1b6b862d52da1f9ef730f12305161ed47
sezahedi <sezahedi13@gmail.com>
2022-09-20 10:46:01 +0430

    Add project files.

---
+++ Inheritance_Step5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Step5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Father father = new Father();
+            father.GetlastName();
+            Boy boy = new Boy();
+            boy.GetEyeColor();
+        }
+    }
+    class Grandpa
+    {
+        public string lastName;
+        public string EyeColor;
+    }
+    class Father : Grandpa
+    {
+        public void GetlastName()
+        {
+            lastName = "Seza";
+            Console.WriteLine(lastName);
+        }
+    }
+    class Boy : Grandpa
+    {
+        public void GetEyeColor()
+        {
+            EyeColor = "Green";
+            Console.WriteLine(EyeColor);
+        }
+    }
+}
